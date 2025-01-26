564c1a273099b837ad3f072eb1337196b0cf8743
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-01-26 15:06:43 -0300

    text-area

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -11,6 +11,8 @@ import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 })
 export class CreateCourseStep1Component {
 
+  SAMPLE_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel cursus elit. Nunc vehicula tincidunt sodales. Mauris id lacus elementum, imperdiet justo sed, dignissim tortor. Vestibulum eget felis odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis diam vel ligula vulputate mattis id eget mi. Etiam semper volutpat fringilla. Vivamus id congue dui. Phasellus rutrum mi risus, et efficitur erat iaculis sed. Vivamus porta et leo sit amet mattis. Integer dictum tincidunt gravida. Nunc massa dui, varius ac nibh et, suscipit luctus mauris. Proin venenatis enim in nulla imperdiet, nec tempor justo imperdiet. Nullam vestibulum mattis mattis.";
+
   form = this.fb.group({
     title: ['', [
       Validators.required,
@@ -21,7 +23,7 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [this.SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
 
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
