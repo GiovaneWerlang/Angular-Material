a92b6e5c9c2466be372d87a72752b361236d46ee
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-02-02 16:57:40 -0300

    expanded

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -33,6 +33,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
     }
 
     displayedColumns = ['seqNo', 'description', 'duration'];
+    expandedLesson: Lesson = null;
 
     ngOnInit() {
 
@@ -60,6 +61,14 @@ export class CourseComponent implements OnInit, AfterViewInit {
         finalize(() => this.loading = false)
       )
       .subscribe();
+    }
+
+    onToggleLesson(lesson: Lesson){
+      if(lesson === this.expandedLesson){
+        this.expandedLesson = null;
+      }else{
+        this.expandedLesson = lesson;
+      }
     }
 
     ngAfterViewInit() {
