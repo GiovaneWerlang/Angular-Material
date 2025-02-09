1bc960c979d4a7d5acef6b2345e013f4329923b3
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-02-09 16:53:00 -0300

    responsive dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -44,6 +44,8 @@ export function openEditCourseDialog(dialog: MatDialog, course: Course){
     const config = new MatDialogConfig();
     config.disableClose = true;
     config.autoFocus = true;
+    config.panelClass = "modal-panel";
+
     config.data = {...course};
 
     const dialogRef = dialog.open(CourseDialogComponent, config);
